66520f771b4d65c44a77f887d268f109167abcc2
林晨 <506430249@qq.com>
2022-11-22 17:10:28 +0800

    reload()方法实现页面数据自动更新

--- share-study-front/src/App.vue
+++ share-study-front/src/App.vue
@@ -8,7 +8,7 @@
         <a-breadcrumb-item>App</a-breadcrumb-item>
       </a-breadcrumb>
       <div>
-        <router-view></router-view>
+        <router-view v-if="isRouterAlive"></router-view>
       </div>
     </a-layout-content>
   </a-layout>
@@ -18,8 +18,24 @@
 import Header from "@/components/Header.vue";
 export default {
   name: "home-view",
+  provide() {
+    // 注册一个方法
+    return {
+      reload: this.reload,
+    };
+  },
   data() {
-    return {};
+    return {
+      isRouterAlive: true,
+    };
+  },
+  methods: {
+    reload() {
+      this.isRouterAlive = false;
+      this.$nextTick(function () {
+        this.isRouterAlive = true;
+      });
+    },
   },
   components: {
     Header,
